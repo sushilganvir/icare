f3959aad077a89562298fb3469c63865c51e77a8
sushilganvir <sushilganvir123@gmail.com>
2022-04-21 17:40:38 +0530

    pages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { SearchComponent } from './pages/search/search.component';
+import { EventsComponent } from './pages/events/events.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HelpComponent } from './pages/help/help.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    SearchComponent,
+    EventsComponent,
+    ProfileComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
